05081e4c3af32570b7ea87bf78dcce44df316c56
zakzakst <e1433104@gmail.com>
2022-10-17 11:25:15 +0900

    4-1-3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const url = ref('https://vuejs.org/');
+const isSendButtonDisabled = ref(true);
 </script>
 
 <template>
-  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
-  <p><a :href="url" target="_blank">Vue.jsのサイト（省略系）</a></p>
-  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsのサイト</a></p>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button></p>
 </template>
