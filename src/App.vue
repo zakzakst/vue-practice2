549a7d6b49fc24354c508ee71ebecd22632f0508
zakzakst <e1433104@gmail.com>
2022-10-19 00:18:22 +0900

    5-1-3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const inputNameBind = ref('しんちゃん');
-const inputNameOn = ref('ななし');
-const onInputName = (event: Event): void => {
+
+const inputName2Way = ref('双方向');
+const onInputName2Way = (event: Event): void => {
   const element = event.target as HTMLInputElement;
-  inputNameOn.value = element.value;
+  inputName2Way.value = element.value;
 }
 </script>
 
 <template>
   <section>
-    <input type="text" v-bind:value="inputNameBind">
-  </section>
-  <section>
-    <input type="text" v-on:input="onInputName">
-    <p>{{ inputNameOn }}</p>
+    <input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way">
+    <p>{{ inputName2Way }}</p>
   </section>
 </template>
