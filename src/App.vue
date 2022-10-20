276c0425eb612608bf86a2d9b971f5f6ab45aa08
zakzakst <e1433104@gmail.com>
2022-10-21 00:58:08 +0900

    6-2-2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const cocktailListInit: string[] = ['ホワイトレディ', 'ブルーハワイ', 'ニューヨーク'];
+const cocktailListInit: {[key: number]: string;} = {
+  2345: 'ホワイトレディ',
+  4412: 'ブルーハワイ',
+  6792: 'ニューヨーク',
+};
 const cocktailList = ref(cocktailListInit);
 </script>
 
 <template>
   <ul>
     <li
-      v-for="cocktailName in cocktailList"
-      v-bind:key="cocktailName"
-    >{{ cocktailName }}</li>
+      v-for="(cocktailName, id) in cocktailList"
+      v-bind:key="'cocktailList' + id"
+    >IDが{{ id }}のカクテルは{{ cocktailName }}</li>
   </ul>
   <ul>
     <li
-      v-for="(cocktailName, index) in cocktailList"
-      v-bind:key="cocktailName"
-    >{{ cocktailName }}(インデックス{{ index }})</li>
+      v-for="(cocktailName, id, index) in cocktailList"
+      v-bind:key="'cocktailListWithIdx' + id"
+    >{{ index + 1 }}: IDが{{ id }}のカクテルは{{ cocktailName }}</li>
   </ul>
 </template>
