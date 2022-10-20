9071949b11ad4bc1fe7b06929cb977a52200499c
zakzakst <e1433104@gmail.com>
2022-10-21 01:16:41 +0900

    6-2-5

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const whiteLadyInit: {
+const cocktailDataListInit: Cocktail[] = [
+  { id: 2345, name: 'ホワイトレディ', price: 1200 },
+  { id: 4412, name: 'ブルーハワイ', price: 1500 },
+  { id: 6792, name: 'ニューヨーク', price: 1100 },
+  { id: 8429, name: 'マティーニ', price: 1500 },
+];
+const cocktailDataList = ref(cocktailDataListInit);
+
+interface Cocktail {
   id: number;
   name: string;
   price: number;
-  recipe: string;
-} = {
-  id: 2345,
-  name: 'ホワイトレディ',
-  price: 1200,
-  recipe: 'テキスト',
-};
-const whiteLady = ref(whiteLadyInit);
+}
 </script>
 
 <template>
-  <dl>
-    <template
-      v-for="(value, key) in whiteLady"
-      v-bind:key="key"
+  <ul>
+    <li
+      v-for="cocktailItem in cocktailDataList"
+      v-bind:key="cocktailItem.id"
     >
-      <dt>{{ key }}</dt>
-      <dd>{{ value }}</dd>
-    </template>
-  </dl>
+      {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
+    </li>
+  </ul>
 </template>
