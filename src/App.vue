6c3262e7a1d6690024a3a36f8e193bae1b900959
zakzakst <e1433104@gmail.com>
2022-10-21 01:03:24 +0900

    6-2-3

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const cocktailListInit: {[key: number]: string;} = {
-  2345: 'ホワイトレディ',
-  4412: 'ブルーハワイ',
-  6792: 'ニューヨーク',
-};
+const cocktailListInit = new Map<number, string>();
+cocktailListInit.set(2345, 'ホワイトレディ');
+cocktailListInit.set(4412, 'ブルーハワイ');
+cocktailListInit.set(6792, 'ニューヨーク');
 const cocktailList = ref(cocktailListInit);
 </script>
 
 <template>
   <ul>
     <li
-      v-for="(cocktailName, id) in cocktailList"
-      v-bind:key="'cocktailList' + id"
+      v-for="[id, cocktailName] in cocktailList"
+      v-bind:key="id"
     >IDが{{ id }}のカクテルは{{ cocktailName }}</li>
-  </ul>
-  <ul>
-    <li
-      v-for="(cocktailName, id, index) in cocktailList"
-      v-bind:key="'cocktailListWithIdx' + id"
-    >{{ index + 1 }}: IDが{{ id }}のカクテルは{{ cocktailName }}</li>
   </ul>
 </template>
