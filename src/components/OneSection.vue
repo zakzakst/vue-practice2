85f758de6d9b28c63b85e489d7f8851f23ad392e
zakzakst <e1433104@gmail.com>
2022-10-24 01:53:12 +0900

    8-1

---
+++ src/components/OneSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="box">
+    <h4>一つのコンポーネント</h4>
+    <p>コンポーネントとは...</p>
+  </section>
+</template>
+
+<style>
+.box {
+  border: green 1px dashed;
+  margin: 10px;
+}
+</style>
