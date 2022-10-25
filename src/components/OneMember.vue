ea07a0c4636cc9e90e11cf371f52891bb379bb52
zakzakst <e1433104@gmail.com>
2022-10-25 11:43:14 +0900

    8-4-3

---
+++ src/components/OneMember.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+interface Props {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+const props = defineProps<Props>();
+const localPoints = ref(props.points);
+const localNote = computed(
+  (): string => {
+    let localNote = props.note;
+    if (localNote == undefined) {
+      localNote = '--';
+    }
+    return localNote;
+  }
+);
+
+const pointUp = (): void => {
+  localPoints.value++;
+}
+</script>
+
+<template>
+  <section class="box">
+    <h4>{{ name }}</h4>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ id }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ email }}</dd>
+      <dt>保有ポイント</dt>
+      <dd>{{ localPoints }}</dd>
+      <dt>備考</dt>
+      <dd>{{ localNote }}</dd>
+    </dl>
+    <button @click="pointUp">ポイント加算</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
