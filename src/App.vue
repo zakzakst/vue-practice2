06f57298dadcdb40511f3f6b2651bf233fa7c571
zakzakst <e1433104@gmail.com>
2022-10-26 10:26:57 +0900

    9-1-5

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 import { ref } from 'vue';
 import OneSection from './components/OneSection.vue';
 
+const taroProblemsInit: string[] = ['電話が通じません', '留守です'];
 const taro = ref('田中太郎');
+const taroProblems = ref(taroProblemsInit);
 const jiro = ref('鈴木二郎');
 </script>
 
 <template>
   <h2>Slotの利用</h2>
   <OneSection :name="taro">
-    <p>{{ taro }}さんは連絡がつきません</p>
+    <ul>
+      <li v-for="problem in taroProblems" :key="problem">{{ problem }}</li>
+    </ul>
   </OneSection>
   <OneSection :name="jiro" />
 </template>
