2243d98df79be642ccf6f3a1d1714567d885ef6b
zakzakst <e1433104@gmail.com>
2022-10-26 10:10:00 +0900

    9-1-2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { reactive, provide } from 'vue';
-import BaseSection from './components/BaseSection.vue';
-import type { Member } from './interfaces';
+import { ref } from 'vue';
+import OneSection from './components/OneSection.vue';
 
-const memberList = new Map<number, Member>();
-memberList.set(33456, {id: 33456, name: '田中太郎', email: 'bow@example.com', points: 35, note: '初回入会特典あり'});
-memberList.set(47783, {id: 47783, name: '鈴木次郎', email: 'mue@example.com', points: 53});
-provide('memberList', reactive(memberList));
+const taro = ref('田中太郎');
 </script>
 
 <template>
-  <BaseSection />
+  <h2>Slotの利用</h2>
+  <OneSection :name="taro">
+    <p>連絡がつきません</p>
+  </OneSection>
 </template>
